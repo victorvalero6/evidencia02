b4015768d8106b3c4afeb739b8d06c40c6168b8b
Victor Valero <victorvalero@Victors-MacBook-Air-3.local>
2025-05-31 00:59:03 -0600

    lista valorItems terminada

--- conversion.rkt
+++ conversion.rkt
@@ -34,14 +34,34 @@
         "powdered sugar" 120 
         "dark chocolate chips" 170
         "sea salt" 288
-        "large eggs" 50
+        "large egg" 50
         "canola oil" 220
         "water" 240
         "vanilla" 13
         "extra-virgin olive oil" 215
         "white wine vinegar" 240
         "minced garlic" 130
-        "driedoregano" 16
-        "redpepperflakes" 48
+        "dried oregano" 16
+        "red pepper flakes" 48
         "smoked paprika" 92
-        "finely chopped fresh flat-leaf parsley" 60))
+        "fresh parsley" 60
+        "dry fettuccine pasta" 100
+        "butter" 227
+        "heavy cream" 240
+        "salt" 288
+        "black pepper" 96
+        "garlic salt" 288
+        "grated romano cheese" 80
+        "grated parmesan cheese" 100
+        "all-purpose flour" 120
+        "almond flour" 96
+        "baking powder" 192
+        "kosher salt" 288
+        "extra-virgin olive oil" 215
+        "grabulated sugar" 200
+        "fresh lemon juice" 240
+        "lemon zest" 2
+        "new york strip steak" 454
+        "vegetable oil" 220
+        "garlic clove" 5
+        "fresh rosemary"))
